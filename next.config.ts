0c9e1132a61ddbcc6349e2afee035fdb061fd22b
mnoor205 <m.noor205@icloud.com>
2025-05-23 00:56:16 -0500

    added posthog

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,24 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['castclip.revolt-ai.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
